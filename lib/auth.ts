d1a610380d4d6ef44a382018290d754662d628a2
Deni Ikbal <deniikbal@gmail.com>
2025-12-31 12:32:51 +0700

    uploaddd

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,12 +10,12 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
-    socialProviders: {
+    socialProviders: process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET ? {
         google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         },
-    },
+    } : {},
     plugins: [
         admin()
     ]
